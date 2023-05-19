9f03580e62048af7a51c87d02c52dc7d48975b2e
Vlad Kovalynskyi <99043529+vladkovalynskyi@users.noreply.github.com>
2023-05-19 23:33:27 +0300

    1 try

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link"
+        href="${original}">
+        <img class="gallery__image"
+        src="${preview}" 
+        data-source="${original}" 
+        alt="${description}" />
+        </a>
+        </li>`;
+    }).join('');
+};
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+gallery.addEventListener('click', onGalleryClick);
+
+function onGalleryClick(evt) {
+    evt.preventDefault();
+
+    const { target } = evt;
+
+    if (!target.classList.contains('gallery__image')) {
+        return;
+    }
+    const originalImageUrl = target.dataset.source;
+
+    if (originalImageUrl) {
+    const instance = basicLightbox.create(`
+      <img src="${originalImageUrl}" width="800" height="600">
+    `);
+        instance.show();
+
+    document.addEventListener('keydown', onKeyDown);
+
+  function onKeyDown(evt) {
+    if (evt.code === 'Escape') {
+      instance.close();
+    }
+  }      
+  }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+        return `
+        <li class="gallery__item">
+        <a class="gallery__link"
+        href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        alt="${description}" />
+        </a>
+        </li>`;
+    }).join('');
+};
+
+gallery.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250,
+});
